ec2cae04359092690ea3b496f1c51b0c4ca8c0f9
samashtar <sam.ashtar@flatironschool.com>
2018-10-05 17:46:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Write your code in this file!
 
 let currentUser = "sam"
-const welcomeMessage = "Welcome to Flatbook, "
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
